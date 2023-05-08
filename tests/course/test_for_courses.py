939fb5783f607f5f492e04e747de6fddf73761ef
Nati Fridman <nati2fridman@gmail.com>
2023-05-08 14:56:15 +0300

    Revert "Renaming some models, fields and files"

--- tests/course/test_for_courses.py
+++ tests/course/test_for_courses.py
@@ -1,5 +1,5 @@
 import pytest
-from course.models import TeacherCourse, StudentCourse
+from course.models import TeacherCourses, StudentCourses
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
@@ -34,17 +34,17 @@ def user_one(db):
 @pytest.fixture
 def teacher_course_zero(db, user_zero):
     user_zero.save()
-    return TeacherCourse(teacher_id=user_zero, course_name=COURSE_NAME, description=DESCRIPTION,
-                         difficulty_level=DIFFICULTY, category=CATEGORY)
+    return TeacherCourses(teacher_id=user_zero, course_name=COURSE_NAME, description=DESCRIPTION,
+                          difficulty_level=DIFFICULTY, category=CATEGORY)
 
 
 # teacher 2
 @pytest.fixture
 def teacher_course_one(db, user_zero):
     user_zero.save()
-    teacher_course = TeacherCourse(teacher_id=user_zero, course_name=COURSE_NAME, description=DESCRIPTION,
-                                   difficulty_level=DIFFICULTY, category=CATEGORY, price=PRICE,
-                                   years_of_experience=YEARS_OF_EXP)
+    teacher_course = TeacherCourses(teacher_id=user_zero, course_name=COURSE_NAME, description=DESCRIPTION,
+                                    difficulty_level=DIFFICULTY, category=CATEGORY, price=PRICE,
+                                    years_of_experience=YEARS_OF_EXP)
     teacher_course.save()
     return teacher_course
 
@@ -52,9 +52,9 @@ def teacher_course_one(db, user_zero):
 # teacher 3
 @pytest.fixture
 def teacher_course_two(db, user_one):
-    teacher_course = TeacherCourse(teacher_id=user_one, course_name=COURSE_NAME, description=DESCRIPTION,
-                                   difficulty_level=DIFFICULTY, category=CATEGORY, price=PRICE,
-                                   years_of_experience=YEARS_OF_EXP)
+    teacher_course = TeacherCourses(teacher_id=user_one, course_name=COURSE_NAME, description=DESCRIPTION,
+                                    difficulty_level=DIFFICULTY, category=CATEGORY, price=PRICE,
+                                    years_of_experience=YEARS_OF_EXP)
     teacher_course.save()
     return teacher_course
 
@@ -62,13 +62,13 @@ def teacher_course_two(db, user_one):
 # student 1
 @pytest.fixture
 def student_course_zero(teacher_course_one, user_one):
-    return StudentCourse(student_id=user_one, teacher_course_id=teacher_course_one)
+    return StudentCourses(student_id=user_one, teacher_course_id=teacher_course_one)
 
 
 # student 2 / TEACHER = USER0 , STUDENT = USER1
 @pytest.fixture
 def student_course_one(db, teacher_course_one, user_one):
-    student_course = StudentCourse(student_id=user_one, teacher_course_id=teacher_course_one)
+    student_course = StudentCourses(student_id=user_one, teacher_course_id=teacher_course_one)
     student_course.save()
     return student_course
 
@@ -77,7 +77,7 @@ def student_course_one(db, teacher_course_one, user_one):
 @pytest.fixture
 def student_course_two(db, teacher_course_two, user_zero):
     user_zero.save()
-    student_course = StudentCourse(student_id=user_zero, teacher_course_id=teacher_course_two)
+    student_course = StudentCourses(student_id=user_zero, teacher_course_id=teacher_course_two)
     student_course.save()
     return student_course
 
@@ -95,71 +95,71 @@ class TestStudentCourse:
         teacher_id1 = user_zero.pk
         teacher_id2 = user_one.pk
 
-        assert course_id1 in StudentCourse.objects.get_teacher_pending(teacher_id1)
-        assert course_id1 not in StudentCourse.objects.get_teacher_pending(teacher_id2)
+        assert course_id1 in StudentCourses.objects.get_teacher_pending(teacher_id1)
+        assert course_id1 not in StudentCourses.objects.get_teacher_pending(teacher_id2)
 
-        assert course_id2 not in StudentCourse.objects.get_teacher_pending(teacher_id1)
-        assert course_id2 in StudentCourse.objects.get_teacher_pending(teacher_id2)
+        assert course_id2 not in StudentCourses.objects.get_teacher_pending(teacher_id1)
+        assert course_id2 in StudentCourses.objects.get_teacher_pending(teacher_id2)
 
     def test_teacher_pending_with_change_to_confirmed(self, student_course_one, user_zero):
         course_id1 = student_course_one.teacher_course_id.course_id
         teacher_id1 = user_zero.pk
 
-        assert course_id1 in StudentCourse.objects.get_teacher_pending(teacher_id1)
+        assert course_id1 in StudentCourses.objects.get_teacher_pending(teacher_id1)
         student_course_one.change_to_confirmed()
-        assert course_id1 not in StudentCourse.objects.get_teacher_pending(teacher_id1)
+        assert course_id1 not in StudentCourses.objects.get_teacher_pending(teacher_id1)
 
     def test_student_pending(self, student_course_one, student_course_two, user_zero, user_one):
         student_id1 = user_one
         student_id2 = user_zero
 
-        assert student_course_one in StudentCourse.objects.get_student_pending(student_id1)
-        assert student_course_one not in StudentCourse.objects.get_student_pending(student_id2)
+        assert student_course_one in StudentCourses.objects.get_student_pending(student_id1)
+        assert student_course_one not in StudentCourses.objects.get_student_pending(student_id2)
 
-        assert student_course_two not in StudentCourse.objects.get_student_pending(student_id1)
-        assert student_course_two in StudentCourse.objects.get_student_pending(student_id2)
+        assert student_course_two not in StudentCourses.objects.get_student_pending(student_id1)
+        assert student_course_two in StudentCourses.objects.get_student_pending(student_id2)
 
     def test_student_pending_with_change_to_confirmed(self, student_course_one, user_one):
         student_id1 = user_one
 
-        assert student_course_one in StudentCourse.objects.get_student_pending(student_id1)
+        assert student_course_one in StudentCourses.objects.get_student_pending(student_id1)
         student_course_one.change_to_confirmed()
-        assert student_course_one not in StudentCourse.objects.get_student_pending(student_id1)
+        assert student_course_one not in StudentCourses.objects.get_student_pending(student_id1)
 
     def test_student_courses(self, student_course_one, student_course_two, user_zero, user_one):
         student_id1 = user_one
         student_id2 = user_zero
 
-        assert student_course_one in StudentCourse.objects.get_student_courses(student_id1)
-        assert student_course_two not in StudentCourse.objects.get_student_courses(student_id1)
+        assert student_course_one in StudentCourses.objects.get_student_courses(student_id1)
+        assert student_course_two not in StudentCourses.objects.get_student_courses(student_id1)
 
-        assert student_course_one not in StudentCourse.objects.get_student_courses(student_id2)
-        assert student_course_two in StudentCourse.objects.get_student_courses(student_id2)
+        assert student_course_one not in StudentCourses.objects.get_student_courses(student_id2)
+        assert student_course_two in StudentCourses.objects.get_student_courses(student_id2)
 
     def test_regitser_to_your_own_course_fails_validation(self, db, teacher_course_one, user_zero):
         with pytest.raises(ValidationError):
-            student_course = StudentCourse(student_id=user_zero, teacher_course_id=teacher_course_one)
+            student_course = StudentCourses(student_id=user_zero, teacher_course_id=teacher_course_one)
             student_course.save()
 
     def test_regitser_to_the_same_course_twice__fails_validation(self, db, teacher_course_one, user_one):
         with pytest.raises(ValidationError):
-            student_course = StudentCourse(student_id=user_one, teacher_course_id=teacher_course_one)
+            student_course = StudentCourses(student_id=user_one, teacher_course_id=teacher_course_one)
             student_course.save()
-            student_course1 = StudentCourse(student_id=user_one, teacher_course_id=teacher_course_one)
+            student_course1 = StudentCourses(student_id=user_one, teacher_course_id=teacher_course_one)
             student_course1.save()
 
     def test_invaled_status_fails_validation(self, db, teacher_course_one, user_one):
         with pytest.raises(ValidationError):
-            student_course = StudentCourse(student_id=user_one, teacher_course_id=teacher_course_one,
-                                           status=INVALID_STATUS)
+            student_course = StudentCourses(student_id=user_one, teacher_course_id=teacher_course_one,
+                                            status=INVALID_STATUS)
             student_course.save()
 
     def test_registering_a_student_without_user_fails_validation(self, db, teacher_course_two, user_zero):
         with pytest.raises(ValidationError):
-            student_course = StudentCourse(student_id=user_zero, teacher_course_id=teacher_course_two)
+            student_course = StudentCourses(student_id=user_zero, teacher_course_id=teacher_course_two)
             student_course.save()
 
     def test_registering_a_student_without_teacher_course_fails_validation(self, db, teacher_course_zero, user_one):
         with pytest.raises(ValidationError):
-            student_course = StudentCourse(student_id=user_one, teacher_course_id=teacher_course_zero)
+            student_course = StudentCourses(student_id=user_one, teacher_course_id=teacher_course_zero)
             student_course.save()
