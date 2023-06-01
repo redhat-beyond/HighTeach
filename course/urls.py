d07b8556f08fa053be22fc086b2798b249fb76bb
Shiraz Yom Tov <shirazyomtov@gmail.com>
2023-06-01 07:32:08 +0300

    Add the option to see reviews in the course page
    The changes made:
    
    1. Integration of HTML code into the existing course page HTML file.
    
    2. Add the option to see all the reviews of specific course
    
    3. Add the option to update a reviews of specific course
    
    4. Add the option to delete a reviews of specific course
    
    5. Add the option to add a reviews of specific course
    
    6. Remove the old html files: create_review and reviews_by_course
    
    7. Update the tests, view function and url

--- course/urls.py
+++ course/urls.py
@@ -4,10 +4,9 @@ from course import views
 
 
 urlpatterns = [
-    path('review/<int:course_id>/create_review', views.CreateReviewView.as_view(), name="create_review"),
-    path('review/<int:course_id>', views.ShowReviews.as_view(), name='reviews'),
-    path('review/delete_review/<int:pk>/', views.DeleteReviewView.as_view(), name="delete_review"),
-    path('review/update_review/<int:pk>/', views.UpdateReviewView.as_view(), name="update_review"),
+    path('<int:course_id>/add_review', views.CreateReviewView.as_view(), name="add_review"),
+    path('<int:course_id>/delete_review', views.DeleteReviewView.as_view(), name="delete_review"),
+    path('<int:course_id>/update_review', views.UpdateReviewView.as_view(), name="update_review"),
     path('', CourseList.as_view(), name='show_courses'),
     path('add', AddCourse.as_view(), name="add_course"),
     path('<int:course_id>', CoursePage.as_view(), name="coursePage"),
